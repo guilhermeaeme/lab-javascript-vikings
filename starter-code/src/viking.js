343add10b141d988bbd73b6e9d84df53c0c76cfa
guilhermeaeme <guilhermeaeme@gmail.com>
2018-11-16 18:37:53 -0200

    Saxon

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -37,7 +37,22 @@ Viking.prototype = Object.create(Soldier.prototype);
 Viking.prototype.constructor = Viking;
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+	Soldier.call(this, health, strength);
+
+	this.receiveDamage = (damage) => {
+		this.health -= damage;
+
+		if(this.health > 0) {
+			return `A Saxon has received ${damage} points of damage`;
+		} else {
+			return `A Saxon has died in combat`;
+		}
+	}
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
 // War
 function War() {}
