30c58eacbd023006e5fd5c5da9981efaeda9f118
guilhermeaeme <guilhermeaeme@gmail.com>
2018-11-16 19:12:48 -0200

    War

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -54,5 +54,60 @@ function Saxon(health, strength) {
 Saxon.prototype = Object.create(Soldier.prototype);
 Saxon.prototype.constructor = Saxon;
 
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function randomSelector(allItems) {
+	return allItems[getRandomInt(0, allItems.length)];
+}
+
 // War
-function War() {}
+function War() {
+	this.vikingArmy = [];
+	this.saxonArmy = [];
+
+	this.addViking = (viking) => {
+		this.vikingArmy.push(viking);
+	}
+
+	this.addSaxon = (saxon) => {
+		this.saxonArmy.push(saxon);
+	}
+
+	this.vikingAttack = () => {
+		let saxon = randomSelector(this.saxonArmy);
+		let viking = randomSelector(this.vikingArmy);
+
+		let result = saxon.receiveDamage(viking.attack());
+
+		this.saxonArmy = this.saxonArmy.filter(item => item.health > 0);
+
+		return result;
+	}
+
+	this.saxonAttack = () => {
+		let saxon = randomSelector(this.saxonArmy);
+		let viking = randomSelector(this.vikingArmy);
+
+		let result = viking.receiveDamage(saxon.attack());
+
+		this.vikingArmy = this.vikingArmy.filter(item => item.health > 0);
+
+		return result;
+	}
+
+	this.showStatus = () => {
+		if(this.saxonArmy.length == 0) {
+			return `Vikings have won the war of the century!`;
+		} else if(this.vikingArmy.length == 0) {
+			return `Saxons have fought for their lives and survive another day...`;
+		} else {
+			return `Vikings and Saxons are still in the thick of battle.`;
+		}
+	}
+
+}
+
